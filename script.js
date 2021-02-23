931d707dd1090819516bd39144a15cf60969c560
Muhammad Ayyub Abdurrahman <13518076@std.stei.itb.ac.id>
2021-02-24 01:28:40 +0700

    bagiannya line

--- script.js
+++ script.js
@@ -80,6 +80,20 @@ function setColor(redval, greenval, blueval, numvertices){
     }
 }
 
+function createVertexfromShape(n, length){
+    var vertex = [];
+    var angle = (2 * Math.PI) / (n);
+    var cur_x = length * Math.cos(angle);
+    var cur_y = length * Math.sin(angle);
+    for (var i = 1; i < n; i++){
+        vertex.push(cur_x);
+        vertex.push(cur_y);
+        cur_x = Math.cos(angle) * cur_x - Math.sin(angle) * cur_y;
+        cur_y = Math.cos(angle) * cur_y + Math.sin(angle) * cur_x;
+    }
+    return vertex
+}
+
 window.onload = function init() {
     canvas = document.getElementById("ourCanvas");
     gl = canvas.getContext('experimental-webgl');
@@ -260,3 +274,127 @@ function render() {
         }, 5000
     );
 }
+
+
+//BAGIANNYA LINES
+
+
+function bindBuffer(vertex_buffer, Index_Buffer, vertices, indices){
+    // Bind appropriate array buffer to it
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+    // Pass the vertex data to the buffer
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    // Unbind the buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    // Bind appropriate array buffer to it
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
+    // // Pass the vertex data to the buffer
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+    // // Unbind the buffer
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+}
+
+function draw(shape){
+    // Clear the canvas
+    gl.clearColor(0.9, 0.5, 0.5, 0.9);
+    // Enable the depth test
+    gl.enable(gl.DEPTH_TEST);
+    // Clear the color buffer bit
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    // Set the view port
+    gl.viewport(0,0,canvas.width,canvas.height);
+    // Draw the square
+    gl.drawElements(gl.LINES, indices.length, gl.UNSIGNED_SHORT,0);
+}
+
+function initShaderProgram(){
+     // Vertex shader source code
+     var vertCode =
+     'attribute vec3 coordinates;' +
+     'attribute vec3 color;'+
+     'varying vec3 vColor;'+
+     'void main(void) {' +
+     ' gl_Position = vec4(coordinates, 1.0);' +
+     'vColor = color;'+
+     '}';
+
+    // Create a vertex shader object
+    var vertShader = gl.createShader(gl.VERTEX_SHADER);
+
+    // Attach vertex shader source code
+    gl.shaderSource(vertShader, vertCode);
+
+    // Compile the vertex shader
+    gl.compileShader(vertShader);
+
+    // Fragment shader source code
+    var fragCode = 'precision mediump float;'+
+                'varying vec3 vColor;'+
+                'void main(void) {'+
+                    'gl_FragColor = vec4(vColor, 1.);'+
+                '}';
+
+    // Create fragment shader object 
+    var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+    // Attach fragment shader source code
+    gl.shaderSource(fragShader, fragCode);
+
+    // Compile the fragmentt shader
+    gl.compileShader(fragShader);
+
+    // Create a shader program object to
+    // store the combined shader program
+    var shaderProgram = gl.createProgram();
+
+    // Attach a vertex shader
+    gl.attachShader(shaderProgram, vertShader);
+
+    // Attach a fragment shader
+    gl.attachShader(shaderProgram, fragShader);
+
+    // Link both the programs
+    gl.linkProgram(shaderProgram);
+
+    // Use the combined shader program object
+    gl.useProgram(shaderProgram);
+    return shaderProgram;
+}
+
+function renderlines(){
+    var vertex_buffer = gl.createBuffer();
+    var Index_Buffer = gl.createBuffer();
+    
+    bindBuffer(vertex_buffer, Index_Buffer, vertex, indices);
+    
+    // Create an empty buffer object and store color data
+    var color_buffer = gl.createBuffer ();
+    gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+    var shaderProgram = initShaderProgram();
+
+    // Bind vertex buffer object
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+
+    // Bind index buffer object
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer); 
+    // Get the attribute location
+    var coord = gl.getAttribLocation(shaderProgram, "coordinates");
+    // Point an attribute to the currently bound VBO
+    gl.vertexAttribPointer(coord, 2, gl.FLOAT, false, 0, 0);
+    // Enable the attribute
+    gl.enableVertexAttribArray(coord);
+
+    // bind the color buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+    // get the attribute location
+    var color = gl.getAttribLocation(shaderProgram, "color");
+    // point attribute to the volor buffer object
+    gl.vertexAttribPointer(color, 3, gl.FLOAT, false,0,0) ;
+    // enable the color attribute
+    gl.enableVertexAttribArray(color);
+
+    draw();
+}
+
