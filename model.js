2581c7d3237e85b48620fe428e828e7f7c72d604
nadiaphi <putrinadia.putna@gmail.com>
2021-02-24 14:27:24 +0700

    data model gambar

---
+++ model.js
@@ -0,0 +1,31 @@
+model = [
+    {
+        "shape": "polygon",
+        "n-side" : "Lima",
+        defaultPolima = [
+            0.3,0.5,0.0,    // x, y, z buat titik pertama
+            0.2,0.5,0.0,    // x, y, z buat titik kedua
+            0.2,0.3,0.0,
+            0.4,0.6,0.0, 
+            0.5,0.5,0.0,
+        ],
+    },
+    {
+        "shape": "polygon",
+        "n-side": "Tiga",
+        defaultPolithree= [
+            -0.2,-0.5,0.0,
+            -0.5,-0.5,0.0,
+            -0.75,-0.6,0.0, 
+        ],
+    },
+    {
+        "shape": "square",
+        defaultSquare= [
+            -0.1,0.1,0.0,
+            -0.1,-0.1,0.0,
+            0.1,-0.1,0.0,
+            0.1,0.1,0.0,
+        ],
+    },
+];
